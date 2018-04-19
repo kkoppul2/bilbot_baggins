b2f360a9bfa43279cb95f680430031b51b7a48d5
Karun Koppula <kkoppula2@gmail.com>
2018-04-18 23:09:52 -0500

    merge message access fixed

--- bilbot_hardware/src/merge.cpp
+++ bilbot_hardware/src/merge.cpp
@@ -3,7 +3,7 @@
 #include "bilbot_hardware/merge.hpp"
 #include "sensor_msgs/JointState.h"
 
-
+using namespace bilbot_hardware;
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "merger");
@@ -12,8 +12,8 @@ int main(int argc, char **argv) {
 
 	bilbot_hardware::merger m;
 
-	ros::Subscriber right = n.subscribe("wheel_state/right", 1, merger::rightCallback, &m);
-	ros::Subscriber left = n.subscribe("wheel_state/left", 1, merger::leftCallback, &m);
+	ros::Subscriber right = n.subscribe("wheel_state/right", 1, &merger::rightCallback, &m);
+	ros::Subscriber left = n.subscribe("wheel_state/left", 1, &merger::leftCallback, &m);
 
 	ros::Publisher merged = n.advertise<sensor_msgs::JointState>("wheel_state/combined", 1);
 
