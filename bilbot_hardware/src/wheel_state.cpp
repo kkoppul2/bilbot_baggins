d958c0267f4e0f27577babee2cb7482cd266a0a9
Karun Koppula <kkoppula2@gmail.com>
2018-04-18 22:24:42 -0500

    encoder with publisher

--- bilbot_hardware/src/wheel_state.cpp
+++ bilbot_hardware/src/wheel_state.cpp
@@ -34,16 +34,16 @@ int main(int argc, char **argv)
 	re_decoder dec(pi, pinA, pinB);
 
 	while (ros::ok()){
-		// sensor_msgs::JointState wheel;
-		// wheel.header.stamp = ros::Time::now();
-		// wheel.header.frame_id = "/world";
+		sensor_msgs::JointState wheel;
+		wheel.header.stamp = ros::Time::now();
+		wheel.header.frame_id = "/world";
 
 		// // //Publish position and velocity to JointState message
 		// wheel.position[0] = 0.0; //dec.getPosition();
 		// wheel.velocity[0] = 0.0; //dec.getVelocity();
 		// wheel.effort[0] = 0.0;
 		ROS_INFO("Reached the loop");
-		// wheel_state.publish(wheel);
+		wheel_state.publish(wheel);
 
 		ros::spinOnce();
 
