08d81dd2faee2908e2bd6e7b80a76cd972fb8019
Karun Koppula <kkoppula2@gmail.com>
2018-04-22 18:59:31 -0500

    added nav stack configured without static map

---
+++ bilbot_nav/include/bilbot_nav/nav_goals.hpp
@@ -0,0 +1,24 @@
+##ifndef NAV_GOALS_H_
+#define NAV_GOALS_H_
+
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+
+namespace bilbot_nav {
+
+class nav_goals
+{
+public:
+	geometry_msgs::PoseStamped goal_;
+
+	void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+	
+};
+
+void nav_goals::goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+	goal_ = *msg;
+}
+
+}
+
+#endif
---
+++ bilbot_nav/src/nav_goals.cpp
@@ -0,0 +1,56 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include "bilbot_nav/nav_goals.hpp"
+#include "geometry_msgs/PoseStamped.h"
+#include "std_msgs/Bool.h"
+
+using namespace bilbot_nav;
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "simple_navigation_goals");
+
+  ros::NodeHandle n;
+
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  //wait for the action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  nav_goals nav;
+
+  ros::Subscriber goal_sub = n.subscribe("goal_position", 1, &nav_goals::goalCallback, &nav);
+
+  ros::Publisher action_status_pub = n.advertise<std_msgs::Bool>("action_status", 1);
+
+  ros::Rate loop(2);
+
+  while (ros::ok()) {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose = nav.goal_;
+
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
+
+    ac.waitForResult();
+
+    std_msgs::Bool status;
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      status.data = true;
+    } else {
+      status.data = false;
+    }
+    action_status_pub.publish(status);
+
+    ros::spinOnce();
+    loop.sleep();
+
+  }
+ 
+  return 0;
+}
